8866a60569ace99a278722096dd68f4e8cdfc6b1
Nikolai Pesudovs <npesudovs@outlook.com>
2024-09-13 10:59:04 +0930

    Rewrote login using v-model instead of refs

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,12 +1,12 @@
 <template>
   <main class="app">
     <div class="login-form">
-        <h1>Login</h1>
-        <br>
-        <input ref="sid" type="text" name="id" id="id" placeholder="Student ID" />
-        <input ref="password" type="password" name="password" id="password" placeholder="Password" />
-        <br>
-        <button v-on:click="test()">Login</button>
+      <h1>Login</h1>
+      <br>
+      <input v-model="sid" type="text" name="id" id="id" placeholder="Student ID" />
+      <input v-model="password" type="password" name="password" id="password" placeholder="Password" />
+      <br>
+      <button v-on:click="test()">Login</button>
     </div>
   </main>
 </template>
@@ -16,17 +16,20 @@
 import axios from 'axios';
 
 export default {
-    methods: {
-      test() {
-        const sid = this.$refs.sid.value;
-        const password = this.$refs.password.value;
-
-        axios.post('http://localhost:8000/login', {
-          sid: sid,
-          password: password
-        })
-      }
+  data() {
+    return {
+      sid: '',
+      password: ''
+    }
+  },
+  methods: {
+    test() {
+      axios.post('http://localhost:8000/login', {
+        sid: this.sid,
+        password: this.password
+      })
     }
   }
+}
 
 </script>
