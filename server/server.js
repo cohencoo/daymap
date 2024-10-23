58dbaa20d9900ede033e4ac805a10c2c1a3cdc3a
Nikolai Pesudovs <npesudovs@outlook.com>
2024-10-23 13:56:15 +1030

    Server returns data for 1 week some of the time

--- server/server.js
+++ server/server.js
@@ -1,124 +1,179 @@
-const express = require("express")
-const cheerio = require("cheerio")
-const puppeteer = require("puppeteer")
-const { error } = require("console")
-const cors = require('cors')
-const port = 8000
-const fs = require('fs')
-const { fail } = require("assert")
-// checking if file exists before loading it
-console.log(fs.existsSync('./config.json'))
-if (fs.existsSync('./config.json')){
-  const config = require('./config.json')
-  console.log(config.sid)
+const express = require("express");
+const cheerio = require("cheerio");
+const puppeteer = require("puppeteer");
+const { error } = require("console");
+const cors = require("cors");
+const port = 8000;
+const fs = require("fs");
+const { fail } = require("assert");
+
+try {
+  const config = require("./config.json");
   var sid = config.sid;
   var password = config.password;
-} else{
+}
+catch {
   var sid = null;
   var password = null;
 }
-// console.log(password)
+console.log(sid)
 
-const app = express()
-app.use(express.json())
-app.use(cors({
-  origin: '*'
-}))
+const app = express();
+app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+  }),
+);
 
+async function findTimetable(page) {
+  const $ = cheerio.load(page);
+  const timetableRaw = $(".diary").children();
+  let timetableArr = [];
+  let dayCount = 0;
+  timetableRaw.each((i, el) => {
+    if (dayCount < 5){
+      switch ($(el).attr("class")) {
+        case "diaryDay":
+          timetableArr.push($(el).text());
+          dayCount++;
+          break;
+        case "L ditm":
+          let classObj = {time: '', class: '', other: ''};
+          $(el).children().each((i, el) => {
+            switch ($(el).attr("class")) {
+            case "t":
+              classObj.time =  $(el).text();
+              break;
+              case "c":
+              classObj.class = $(el).text();
+              break;
+            default:
+              classObj.other = $(el).text();
+              break;
+          }
+          });
+          timetableArr.push(classObj);
+          break;
+      default:
+        break;
+      }
+    }
+  })
+  return timetableArr;
+}
 
 app.get("/", (req, res) => {
-    res.send(sid)
-})
+  res.send("hello world");
+});
 
 app.post("/login", (req, res) => {
-  console.log(req.body)
-  let {sid, password} = req.body
-  if (!sid || !password){
-    res.status(400).send("Missing SID or Password")
+  // console.log(req.body);
+  let { sid, password } = req.body;
+  if (!sid || !password) {
+    res.status(400).send("Missing SID or Password");
+  } else {
+    res.send("Success");
   }
-  else{
-    res.send("Success")
-  }
-  fs.writeFile('./config.json', JSON.stringify({sid, password}), (err) => {
+  fs.writeFile("./config.json", JSON.stringify({ sid, password }), (err) => {
     if (err) throw err;
-    console.log('Data written to file');
+    // console.log("Data written to file");
   });
-})
+});
 
-app.get('/scrape', async (req, res) => {
+app.get("/scrape", async (req, res) => {
   // Starting a Puppeteer instance
-  const browser = await puppeteer.launch({headless: false});
+  const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
-  // await page.setDefaultNavigationTimeout(1500); 
+  await page.setDefaultNavigationTimeout(1500);
   // Navigating to the page and handling Auth
   let failCount = 0;
-  while (failCount<3){
-    try{
-      await page.goto('https://gihs.daymap.net/daymap/student/classdiary.aspx');
-      console.log("navigated to daymap");
-      await page.waitForResponse(response => response.status() === 200);
-      console.log("network idle");
+  let success = false;
+  while (failCount < 3 && success == false) {
+    try {
+      console.log(failCount);
+      await page.goto("https://gihs.daymap.net/daymap/student/dayplan.aspx");
+      // console.log("navigated to daymap");
+      await page.waitForResponse((response) => response.status() === 200);
+      // console.log("network idle");
       let submit;
-      switch(await page.url()){
-        case "https://portal.edpass.sa.edu.au/":
-          console.log("portal");
-          const gihs = await page.waitForSelector('tr.idp:nth-child(166)')
+      const url = await page.url() 
+      const parsedUrl = new URL(url);
+      const baseUrl = `${parsedUrl.protocol}//${parsedUrl.hostname}`;
+      console.log(baseUrl);
+      switch (baseUrl) {
+        case "https://portal.edpass.sa.edu.au":
+          const gihs = await page.waitForSelector("tr.idp:nth-child(166)");
           await gihs.click();
-          const cont = await page.waitForSelector('.continue_button');
+          const cont = await page.waitForSelector(".continue_button");
           await cont.click();
-          await page.waitForNavigation();
-          await page.waitForSelector('#okta-signin-username')
-          if (!{sid, password}){
-            throw error('Missing Username and/or password')
+          // await page.waitForNavigation();
+          await page.waitForSelector("#okta-signin-username");
+          if (!{ sid, password }) {
+            throw error("Missing Username and/or password");
+          } else {
+            await page.type("#okta-signin-username", sid);
+            await page.type("#okta-signin-password", password);
           }
-          else{
-            await page.type('#okta-signin-username', sid);
-            await page.type('#okta-signin-password', password);
-          }
-          submit = await page.waitForSelector('.button.button-primary');
+          submit = await page.waitForSelector(".button.button-primary");
           await submit.click();
           break;
         case "https://edpass-0927.okta.com/":
           console.log("okta");
-          await page.waitForSelector('#okta-signin-username')
-          if (!{sid, password}){
-            throw error('Missing Username and/or password')
+          await page.waitForSelector("#okta-signin-username");
+          if (!{ sid, password }) {
+            throw error("Missing Username and/or password");
+          } else {
+            await page.type("#okta-signin-username", sid);
+            await page.type("#okta-signin-password", password);
           }
-          else{
-            await page.type('#okta-signin-username', sid);
-            await page.type('#okta-signin-password', password);
-          }
-          submit = await page.waitForSelector('.button.button-primary');
+          submit = await page.waitForSelector(".button.button-primary");
           await submit.click();
           break;
+        case "https://gihs.daymap.net/":
+          success = true;
+          break;
         default:
-          throw error("unknown website")
-        }
-      await page.waitForNetworkIdle();
-      console.log('idle')
-      if (await page.url() == "https://gihs.daymap.net/daymap/student/dayplan.aspx") {
-        console.log('daymap')
-        break;
+          throw error(`unknown website ${baseUrl}`);
       }
-      else{
+      await page.waitForNetworkIdle();
+      // console.log('idle')
+      if (page.url() == "https://gihs.daymap.net/daymap/student/dayplan.aspx") {
+        // console.log('daymap')
+        break;
+      } else {
         failCount++;
       }
-    }
-    catch {
+    } catch {
       failCount++;
-    }}
-    if (failCount == 3){
-      res.status(500).send("Failed to login")
-      await browser.close();
-      throw error("Failed to login");
     }
-    
-    console.log('escaped' + failCount);
-    await page.waitForNetworkIdle();
-    const diaryContent = await page.content();
+  }
+  if (failCount >= 3) {
     await browser.close();
-    // console.log(content);
+    res.status(500).send("Failed to login");
+  }
+
+  // console.log('escaped' + failCount);
+  await page.waitForNetworkIdle();
+  
+  
+  const pageToday = await page.content();
+  // const weekAway = new Date(Date.now() + 604800000);
+  // var wYear = weekAway.getFullYear();
+  // var wMonth = (weekAway.getMonth() + 1).toString().padStart(2, "0");
+  // var wDay = weekAway.getDate().toString().padStart(2, "0");
+  // console.log(wMonth, wYear, wDay);
+  // await page.focus('#ctl00_cp_dtbDate')
+  // await page.keyboard.type(`${wMonth}/${wDay}/${wYear}`)
+  // const pageWeek = await page.content();
+  await browser.close();
+
+  // Cheerio Content Processing.
+  const timetableToday = await findTimetable(pageToday);
+  // const timetableWeek = await findTimetable(pageWeek);
+  console.log(timetableToday);
+  res.status(200).json({timetable: timetableToday});
 });
 
-app.listen(port)
-console.log(`Server is running on http://localhost:${port}`)
+app.listen(port);
+console.log(`Server is running on http://localhost:${port}`);
